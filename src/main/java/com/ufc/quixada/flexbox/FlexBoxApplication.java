bda1ba3bb1a218798507ef043aedc15ab139634b
Christian Estevam <christianestevamb@gmail.com>
2024-03-31 15:39:01 -0300

    setup

---
+++ src/main/java/com/ufc/quixada/flexbox/FlexBoxApplication.java
@@ -0,0 +1,13 @@
+package com.ufc.quixada.flexbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlexBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufc/quixada/flexbox/FlexBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufc.quixada.flexbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
